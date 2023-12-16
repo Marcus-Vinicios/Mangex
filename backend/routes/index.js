c21c80a9a404f1a83750739566a3ad9580dd1bef
Marcus-Vinicios <marcusv.fl.oliveira@gmail.com>
2023-12-16 13:56:30 -0300

    feat: updating routes #1

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const ListController = require("../controllers/ListController");
 
-router.post('/', ListController.createList);
+router.get('/', ListController.getMyLists);
+router.post('/create/list', ListController.createList);
 
 module.exports = router;
